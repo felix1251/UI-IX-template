f17cf9c7a019311443a3e74da7f1a5cc10ef622d
felix1251 <domhack123@gmail.com>
2022-08-03 20:01:18 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main">
+    <div class="mobile-phone">
+      <div class="brove"><span class="speaker"></span></div>
+      <div class="screen">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.main {
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center
 }
 
-nav {
-  padding: 30px;
+.mobile-phone {
+  margin: auto;
+  padding: 10px 10px 30px;
+  width: 330px;
+  height: 660px;
+  box-shadow: 0 0 20px #e2e2e2;
+  border-radius: 30px;
+  background: black;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.screen {
+  width: 100%;
+  height: 100%;
+  background: #f2f2f2;
+  border-radius: 30px;
+  overflow-y: auto;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.brove {
+  width: 150px;
+  height: 20px;
+  background: black;
+  position: absolute;
+  margin: 0 90px;
+  border-radius: 0 0 20px 20px;
+}
+
+.speaker {
+  width: 60px;
+  height: 5px;
+  background: #d2d2d2;
+  display: block;
+  margin: auto;
+  margin-top: 5px;
+  border-radius: 20px;
 }
 </style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>Content</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
+
+<style scoped>
+  .home{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center
+  }
+</style>
